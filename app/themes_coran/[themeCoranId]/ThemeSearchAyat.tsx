1e15eb3e5e4018918c5822c810ecdf657501b173
Mika <ext.mbenarab@librasoft.fr>
2024-05-05 19:55:17 +0200

    selected sourate

--- app/themes_coran/[themeCoranId]/ThemeSearchAyat.tsx
+++ app/themes_coran/[themeCoranId]/ThemeSearchAyat.tsx
@@ -1,30 +1,39 @@
 import { prisma } from "@/prisma/client";
-
-
+import { SelectAyat } from "./searchAyatSelect";
 
 export default async function ThemeSearchAyat() {
+  const coran = await prisma.quran.findMany({
+    where: { ayah: { gt: 0 } },
+    orderBy: { surah: "asc" },
+  });
+  const ayatsBySourates = new Array<{
+    surahLabel: string;
+    sourateNumber: number | null;
+    ayahs: { number: number | null }[];
+  }>();
 
-    const coran = await prisma.quran.findMany();
-    const ayatsBySourates = new Array<{ surahLabel: string, sourateNumber: number | null, ayahs: { number: number | null }[] }>
+  const getKey = (sourateName: string | null, sourateNumber: number | null) => {
+    return sourateName + " (" + sourateNumber + ")";
+  };
 
-    const getKey = (sourateName: string | null, sourateNumber: number | null) => {
-        return sourateName + " (" + sourateNumber + ")"
+  for (const c of coran) {
+    const ayatsBySourate = ayatsBySourates.find(
+      (a) => a.surahLabel === getKey(c.surrahname, c.surah)
+    );
+    if (!ayatsBySourate) {
+      ayatsBySourates.push({
+        surahLabel: getKey(c.surrahname, c.surah),
+        sourateNumber: c.surah,
+        ayahs: [{ number: c.ayah }],
+      });
+    } else {
+      ayatsBySourate?.ayahs.push({ number: c.ayah });
     }
+  }
 
-    for (const c of coran) {
-        const ayatsBySourate = ayatsBySourates.find(a => a.surahLabel === getKey(c.surrahname, c.surah))
-        if (!ayatsBySourate) {
-            ayatsBySourates.push({ surahLabel: getKey(c.surrahname, c.surah), sourateNumber: c.surah, ayahs: [{ number: c.ayah }] })
-        } else {
-            ayatsBySourate?.ayahs.push({ number: c.ayah })
-        }
-    }
-
-    return (<div>
-
-
-    </div>)
-
-
-
+  return (
+    <div>
+      <SelectAyat ayatsBySourates={ayatsBySourates} />
+    </div>
+  );
 }
--- app/themes_coran/[themeCoranId]/searchAyatSelect.tsx
+++ app/themes_coran/[themeCoranId]/searchAyatSelect.tsx
@@ -1,34 +1,63 @@
-"use client"
+"use client";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select"
-import { useState } from "react"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
 
 type props = {
-    ayatsBySourates: Array<{ surahLabel: string, sourateNumber: number | null, ayahs: { number: number | null }[] }>
-}
+  ayatsBySourates: Array<{
+    surahLabel: string;
+    sourateNumber: number | null;
+    ayahs: { number: number | null }[];
+  }>;
+};
 
 export const SelectAyat = ({ ayatsBySourates }: props) => {
+  const [sourateSelected, setSourateSelected] = useState<number | null>(null);
 
-    const [sourateSelected, setSourateSelected] = useState<number | null>(null)
-    return (
-        <div>
-            <Select onValueChange={(value) => setSourateSelected(Number(value))}>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Sourate" />
-                </SelectTrigger>
-                <SelectContent>
-                    {ayatsBySourates.map(s => <SelectItem value={s.sourateNumber != null ? s.sourateNumber.toString() : ""} key={s.sourateNumber}>{s.surahLabel}</SelectItem>)}
-                </SelectContent>
-            </Select>
-        </div>
+  const ayatsBySourate = ayatsBySourates
+    .find((a) => a.sourateNumber === sourateSelected)
+    ?.ayahs.filter((a) => a.number !== null)
+    // @ts-ignore
+    .sort((a, b) => a.number - b.number);
+  const ayats = ayatsBySourate ? ayatsBySourate.sort() : [];
 
-    )
-
-
-
-}
+  return (
+    <div className="flex gap-2">
+      <Select onValueChange={(value) => setSourateSelected(Number(value))}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Sourate" />
+        </SelectTrigger>
+        <SelectContent>
+          {ayatsBySourates.map((s) => (
+            <SelectItem
+              value={s.sourateNumber != null ? s.sourateNumber.toString() : ""}
+              key={s.sourateNumber}
+            >
+              {s.surahLabel}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Ayat" />
+        </SelectTrigger>
+        <SelectContent>
+          {ayats.map((a) => (
+            <SelectItem
+              key={a.number}
+              value={a.number == null ? "" : a.number.toString()}
+            >
+              {a.number}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
