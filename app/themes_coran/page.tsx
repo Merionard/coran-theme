f24b27a79ed3dc454a3359d0179afbb134975123
Mika <ext.mbenarab@librasoft.fr>
2024-05-09 01:27:01 +0200

    themes et all sous themes

--- app/themes_coran/page.tsx
+++ app/themes_coran/page.tsx
@@ -1,26 +1,64 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { prisma } from "@/prisma/client";
-import Link from "next/link";
+import { Prisma } from "@prisma/client";
 import { NewThemeDialogForm } from "../../components/clientComponents/theme/newThemeDialogForm";
 import { createNewThemeCoran } from "../../components/serverActions/themeCoranAction";
-import { Pencil } from "lucide-react";
-import { ThemeItem } from "@/components/clientComponents/theme/themeItem";
+import Link from "next/link";
+
+export const themeWithSubThemes =
+  Prisma.validator<Prisma.theme$subThemesArgs>()({
+    include: {
+      subThemes: true,
+    },
+  });
+export type ThemeWithSubThemes = Prisma.themeGetPayload<
+  typeof themeWithSubThemes
+>;
 
 export default async function Page() {
-  const Themes = await prisma.theme.findMany({ where: { parent: null } });
+  const themes = await prisma.theme.findMany({
+    where: { parentId: null },
+    include: { subThemes: true },
+  });
+
+  const test = async (theme: ThemeWithSubThemes, subLevel: number) => {
+    if (theme.subThemes.length > 0) {
+      const subThemes = await prisma.theme.findMany({
+        where: { parentId: theme.id },
+        include: { subThemes: true },
+      });
+      const className = "ml-" + subLevel + " pl-4";
+      return (
+        <div className={className}>
+          <Link
+            href={`/themes_coran/${theme.id}`}
+            className={theme.parentId === null ? "font-bold text-xl" : ""}
+          >
+            {theme.name}
+          </Link>
+          {subThemes.map((s) => test(s, subLevel + 2))}
+        </div>
+      );
+    } else {
+      return (
+        <div className={`ml-${subLevel} pl-4`}>
+          <Link
+            href={`/themes_coran/${theme.id}`}
+            className={theme.parentId === null ? "font-bold text-xl" : ""}
+          >
+            {theme.name}
+          </Link>
+        </div>
+      );
+    }
+  };
 
   return (
     <div>
       <NewThemeDialogForm onSubmitForm={createNewThemeCoran} />
 
       <Card className="mt-5">
-        <CardContent>
-          <ul>
-            {Themes.map((t) => (
-              <ThemeItem theme={t} key={t.id} />
-            ))}
-          </ul>
-        </CardContent>
+        <CardContent>{themes.map((t) => test(t, 0))}</CardContent>
       </Card>
     </div>
   );
