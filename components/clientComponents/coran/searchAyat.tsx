17a0e2124bd1e85f6b1fe79c2e5bd14b98c45f0e
Mika <ext.mbenarab@librasoft.fr>
2024-05-21 23:17:18 +0200

    search ayat suite

--- components/clientComponents/coran/searchAyat.tsx
+++ components/clientComponents/coran/searchAyat.tsx
@@ -1,16 +1,69 @@
 "use client";
 
-import { searchAyats } from "@/components/serverActions/coranAction";
+import {
+  AyatWithTitre,
+  searchAyats,
+} from "@/components/serverActions/coranAction";
 import { SearchInput } from "@/components/ui/searchInput";
 import { useState } from "react";
 
 export const SearchAyat = () => {
   const [search, setSearch] = useState("");
+  const [ayats, setAyats] = useState<AyatWithTitre[]>([]);
 
   const onSetSearch = async (value: string) => {
     setSearch(value);
     const ayats = await searchAyats(value);
+    setAyats(ayats);
+  };
+  const highlightSearchTerm = (
+    text: string,
+    searchTerm: string
+  ): JSX.Element => {
+    if (!searchTerm.trim()) {
+      return <>{text}</>;
+    }
+    const harakats = /[\u064B-\u0652]/g;
+    const textWithoutHarakat = text.replace(harakats, "");
+    const searhWithoutHarakats = searchTerm.replace(harakats, "");
+
+    const regex = new RegExp(`(${searhWithoutHarakats})`, "gi");
+
+    const parts = textWithoutHarakat.split(regex);
+
+    return (
+      <>
+        {parts.map((part, index) =>
+          regex.test(part.replace(harakats, "")) ? (
+            <span key={index} className="bg-yellow-400">
+              {part}
+            </span>
+          ) : (
+            part
+          )
+        )}
+      </>
+    );
   };
 
-  return <SearchInput onSearch={onSetSearch} search={search} />;
+  return (
+    <div>
+      <SearchInput onSearch={onSetSearch} search={search} />
+      <div className="mt-5 space-y-5">
+        {ayats.map((a) => (
+          <div key={a.id} className="border-b-2 space-y-3 bg-card">
+            <div className="">
+              {" "}
+              Sourate {a.sourate_number} verset {a.number}{" "}
+              <span>{a.titre}</span>
+            </div>
+            <p className="text-2xl text-right">
+              {highlightSearchTerm(a.content, search)}
+            </p>
+            <p>{a.traduction}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- components/serverActions/coranAction.tsx
+++ components/serverActions/coranAction.tsx
@@ -3,16 +3,23 @@
 import { prisma } from "@/prisma/client";
 import { ayat } from "@prisma/client";
 
+export type AyatWithTitre = ayat & {
+  titre: string;
+};
+
 export const searchAyats = async (search: string) => {
   const harakats = /[\u064B-\u0652]/g;
+  const searhWithoutHarakats = search.replace(harakats, "");
 
-  const result = await prisma.$queryRaw<ayat[]>`
-  SELECT * FROM "ayat"
-  WHERE REGEXP_REPLACE("content", '[\u064B-\u0652]', '', 'g') ILIKE ${
-    "%" + search + "%"
-  }
+  const result = await prisma.$queryRaw<AyatWithTitre[]>`
+  SELECT a.*, s.titre  FROM "ayat" a
+  JOIN "sourate" s ON a.sourate_number = s.number
+  WHERE REGEXP_REPLACE(a."content", '[\u064B-\u0652]', '', 'g') ILIKE ${
+    "%" + searhWithoutHarakats + "%"
+  } order by s.number, a.number
 `;
 
   console.log(result);
+
   return result;
 };
