1233f7f6bb22704dad3011131c130cafe78f495e
Mika <ext.mbenarab@librasoft.fr>
2024-05-22 16:57:04 +0200

    search ayat

--- components/clientComponents/coran/searchAyat.tsx
+++ components/clientComponents/coran/searchAyat.tsx
@@ -4,17 +4,52 @@ import {
   AyatWithTitre,
   searchAyats,
 } from "@/components/serverActions/coranAction";
+import { Button } from "@/components/ui/button";
 import { SearchInput } from "@/components/ui/searchInput";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 
 export const SearchAyat = () => {
   const [search, setSearch] = useState("");
   const [ayats, setAyats] = useState<AyatWithTitre[]>([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const pageSize = 10;
+
+  const fetchAyats = async (searchTerm: string, pageNumber: number) => {
+    const result = await searchAyats(searchTerm, pageNumber, pageSize);
+    setAyats(result.ayats);
+    setTotalPages(Math.ceil(result.totalCount / pageSize));
+    setPage(pageNumber);
+  };
 
   const onSetSearch = async (value: string) => {
     setSearch(value);
-    const ayats = await searchAyats(value);
-    setAyats(ayats);
+    if (value === "") {
+      setAyats([]);
+      setPage(1);
+      setTotalPages(1);
+      return;
+    }
+    setPage(1); // Reset to first page on new search
+    await fetchAyats(value, 1);
+  };
+
+  const handlePreviousPage = async () => {
+    if (page > 1) {
+      const newPage = page - 1;
+      setPage(newPage);
+      await fetchAyats(search, newPage);
+    }
+  };
+
+  const handleNextPage = async () => {
+    if (page < totalPages) {
+      const newPage = page + 1;
+      setPage(newPage);
+      await fetchAyats(search, newPage);
+    }
   };
   const highlightSearchTerm = (
     text: string,
@@ -28,7 +63,6 @@ export const SearchAyat = () => {
     const searhWithoutHarakats = searchTerm.replace(harakats, "");
 
     const regex = new RegExp(`(${searhWithoutHarakats})`, "gi");
-
     const parts = textWithoutHarakat.split(regex);
 
     return (
@@ -46,9 +80,75 @@ export const SearchAyat = () => {
     );
   };
 
+  const getPagninationBtn = () => {
+    const buttons = [];
+    if (totalPages <= 10) {
+      for (let i = 1; i <= totalPages; i++) {
+        buttons.push(
+          <Button
+            key={i}
+            variant={i === page ? "default" : "secondary"}
+            onClick={() => fetchAyats(search, i)}
+          >
+            {i}
+          </Button>
+        );
+      }
+      return buttons;
+    } else {
+      const indexPages = [1, 2, 3, totalPages];
+      if (!indexPages.includes(page)) {
+        indexPages.push(page);
+        if (page > 3) {
+          indexPages.splice(0, 3);
+          indexPages.push(page - 1);
+          indexPages.push(page - 2);
+          indexPages.push(page - 3);
+        }
+        indexPages.sort((a, b) => a - b);
+      }
+      return indexPages.map((i) => (
+        <Button
+          key={i}
+          variant={i === page ? "default" : "secondary"}
+          onClick={() => fetchAyats(search, i)}
+        >
+          {i}
+        </Button>
+      ));
+    }
+  };
+
   return (
     <div>
-      <SearchInput onSearch={onSetSearch} search={search} />
+      <div className="flex justify-end">
+        <SearchInput
+          onSearch={onSetSearch}
+          search={search}
+          placeHolder="Tappez votre recherche"
+        />
+      </div>
+      {ayats.length > 0 && (
+        <div className="flex justify-center mt-5 gap-3 items-center">
+          <Button
+            onClick={handlePreviousPage}
+            size={"icon"}
+            disabled={page === 1}
+            className="disabled:bg-gray-400"
+          >
+            <ChevronLeft />
+          </Button>
+          {getPagninationBtn()}
+          <Button
+            onClick={handleNextPage}
+            size={"icon"}
+            disabled={page === totalPages}
+            className="disabled:bg-gray-400"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
       <div className="mt-5 space-y-5">
         {ayats.map((a) => (
           <div key={a.id} className="border-b-2 space-y-3 bg-card">
--- components/serverActions/coranAction.tsx
+++ components/serverActions/coranAction.tsx
@@ -7,19 +7,41 @@ export type AyatWithTitre = ayat & {
   titre: string;
 };
 
-export const searchAyats = async (search: string) => {
+export const searchAyats = async (
+  search: string,
+  page: number,
+  pageSize: number
+) => {
   const harakats = /[\u064B-\u0652]/g;
-  const searhWithoutHarakats = search.replace(harakats, "");
+  const searchWithoutHarakats = search.replace(harakats, "");
 
-  const result = await prisma.$queryRaw<AyatWithTitre[]>`
-  SELECT a.*, s.titre  FROM "ayat" a
-  JOIN "sourate" s ON a.sourate_number = s.number
-  WHERE REGEXP_REPLACE(a."content", '[\u064B-\u0652]', '', 'g') ILIKE ${
-    "%" + searhWithoutHarakats + "%"
-  } order by s.number, a.number
-`;
+  const offset = (page - 1) * pageSize;
 
-  console.log(result);
+  const [result, totalcount] = await prisma.$transaction([
+    prisma.$queryRaw<AyatWithTitre[]>`
+      SELECT a.*, s.titre
+      FROM "ayat" a
+      JOIN "sourate" s ON a.sourate_number = s.number
+      WHERE REGEXP_REPLACE(a."content", '[\u064B-\u0652]', '', 'g') ILIKE ${
+        "%" + searchWithoutHarakats + "%"
+      }
+      ORDER BY s.number, a.number
+      LIMIT ${pageSize}
+      OFFSET ${offset}
+    `,
+    prisma.$queryRaw<number>`
+      SELECT COUNT(*) as totalcount
+      FROM "ayat" a
+      JOIN "sourate" s ON a.sourate_number = s.number
+      WHERE REGEXP_REPLACE(a."content", '[\u064B-\u0652]', '', 'g') ILIKE ${
+        "%" + searchWithoutHarakats + "%"
+      }
+    `,
+  ]);
 
-  return result;
+  return {
+    ayats: result,
+    //@ts-ignore
+    totalCount: Number(totalcount[0].totalcount),
+  };
 };
