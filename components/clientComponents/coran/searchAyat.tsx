a72d7ccd3a89a696d0b006ee6c26a421bc2966d4
Mika <ext.mbenarab@librasoft.fr>
2024-05-22 20:11:07 +0200

    delay sssearch

--- components/clientComponents/coran/searchAyat.tsx
+++ components/clientComponents/coran/searchAyat.tsx
@@ -137,7 +137,7 @@ export const SearchAyat = ({ ifSomeSearch }: props) => {
     }
   };
 
-  const majSearchValue = useDebounce((val: string) => onSetSearch(val), 3000);
+  const majSearchValue = useDebounce((val: string) => onSetSearch(val), 2000);
   const onSearch = (value: string) => {
     setSearch(value);
     majSearchValue(value);
